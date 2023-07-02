48e300e9f8e786015ed8f89828ea937c7da7837e
Isaac Leonardo M <mcisaacleonardo@gmail.com>
2023-07-02 15:37:49 -0400

    Adding interactions to open and close shopping cart menu

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const desktopMenu = $('.menu-container')
 const burguerButton = $('.hamburguer-menu')
 const closeButton = $('.close-icon')
 const mobileMenu = $('.menu-mobile')
+const shoppingCartButton = $('.shopping-cart-icon')
+const arrowButton = $('.arrow-shoppin-cart-icon')
+const myOrderMenu = $('.my-order')
 
 // Toggle desktop menu when clicking account button
 accountButton.addEventListener('click', () => desktopMenu.classList.toggle('inactive'))
@@ -12,3 +15,7 @@ accountButton.addEventListener('click', () => desktopMenu.classList.toggle('inac
 // Toggle mobile menu when clicking hamburguer and close button
 burguerButton.addEventListener('click', () => mobileMenu.classList.remove('inactive'))
 closeButton.addEventListener('click', () => mobileMenu.classList.add('inactive'))
+
+// Toggle my order menu
+shoppingCartButton.addEventListener('click', () => myOrderMenu.classList.remove('inactive'))
+arrowButton.addEventListener('click', () => myOrderMenu.classList.add('inactive'))
