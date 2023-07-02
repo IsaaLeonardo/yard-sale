3b452846a7c5b596824145e4a6cff3785007d8f7
Isaac Leonardo M <mcisaacleonardo@gmail.com>
2023-07-02 12:15:01 -0400

    Add mobile menu open and close when clicking respective buttons

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ const $ = (selector) => document.querySelector(selector);
 
 const accountButton = $('.my-account')
 const desktopMenu = $('.menu-container')
+const burguerButton = $('.hamburguer-menu')
+const closeButton = $('.close-icon')
+const mobileMenu = $('.menu-mobile')
 
 // Toggle desktop menu when clicking account button
 accountButton.addEventListener('click', () => desktopMenu.classList.toggle('inactive'))
+
+// Toggle mobile menu when clicking hamburguer and close button
+burguerButton.addEventListener('click', () => mobileMenu.classList.remove('inactive'))
+closeButton.addEventListener('click', () => mobileMenu.classList.add('inactive'))
